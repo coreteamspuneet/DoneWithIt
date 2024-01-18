45352724ea816a3f5ba20e052df5834de0d836ff
Puneet Vaishnav <puneet@coreteams.us>
2024-01-18 13:34:15 +0530

    updated

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 
 export default function App() {
+  const handelPress = () => {
+    console.log('text clicked');
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handelPress}>Hello</Text>
+      <Image source={require('./assets/favicon.png')}/>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'dodgerblue',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    // backgroundColor:'green',
+    // fontSize:'20rem'
+    // fontSize:'10px',
   },
 });
